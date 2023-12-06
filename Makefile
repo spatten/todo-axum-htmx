1cdc78c0e14a5227dd41073f7820c54bdf1963f3
spatten <scott@fossa.com>
2023-12-06 00:38:53 -0800

    get auto-reload working properly

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 # run the server in watch mode
 watch:
 	echo "server is running on http://localhost:3000" && \
-	systemfd --no-pid -s http::5000 -- cargo watch -x run
+	systemfd --no-pid -s http::3000 -- cargo watch -x run
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 use axum::{extract::Query, response::Html, routing::get, Router};
+use listenfd::ListenFd;
 use serde::Deserialize;
+use tokio::net::TcpListener;
 use tower_http::services::{ServeDir, ServeFile};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+    // Serve files from the client directory, falling back to client/404.html
     let serve_dir = ServeDir::new("client").not_found_service(ServeFile::new("client/404.html"));
+
+    // Respond to GET /search, otherwise attempt to serve the file from the client directory
     let app = Router::new()
         .route("/search", get(search))
         .fallback_service(serve_dir);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // Auto-reload if you use `make watch`: https://github.com/tokio-rs/axum/blob/main/examples/auto-reload/src/main.rs
+    let mut listenfd = ListenFd::from_env();
+    let listener = match listenfd.take_tcp_listener(0).unwrap() {
+        // if we are given a tcp listener on listen fd 0, we use that one
+        Some(listener) => TcpListener::from_std(listener).unwrap(),
+        // otherwise fall back to local listening
+        None => TcpListener::bind("127.0.0.1:3000").await.unwrap(),
+    };
     axum::serve(listener, app).await.unwrap();
 }
 
