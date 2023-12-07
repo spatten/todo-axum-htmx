efa76296eee553e1e26b5c8f79fd86ab59045af9
spatten <scott@fossa.com>
2023-12-06 21:31:32 -0800

    make a query

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use axum::{extract::Query, response::Html, routing::get, Router};
+use axum::{
+    extract::Query, extract::State, http::StatusCode, response::Html, routing::get, Router,
+};
 use listenfd::ListenFd;
 use serde::Deserialize;
-use sqlx::postgres::PgPoolOptions;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use tokio::net::TcpListener;
 use tower_http::services::{ServeDir, ServeFile};
 
@@ -27,9 +29,11 @@ async fn main() {
     let serve_dir = ServeDir::new("client").not_found_service(ServeFile::new("client/404.html"));
 
     // Respond to GET /search, otherwise attempt to serve the file from the client directory
+    // Also, add our postgres pool to the state so that our routes can use it
     let app = Router::new()
         .route("/search", get(search))
-        .fallback_service(serve_dir);
+        .fallback_service(serve_dir)
+        .with_state(pool);
 
     // Auto-reload if you use `make watch`: https://github.com/tokio-rs/axum/blob/main/examples/auto-reload/src/main.rs
     let mut listenfd = ListenFd::from_env();
@@ -51,18 +55,39 @@ async fn main() {
         .expect("should be able to serve");
 }
 
+/// Utility function for mapping any error into a `500 Internal Server Error`
+/// response.
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+}
+
 #[derive(Debug, Deserialize)]
 struct SearchParams {
     search: String,
 }
 
-async fn search(Query(params): Query<SearchParams>) -> Html<String> {
+async fn search(
+    Query(params): Query<SearchParams>,
+    State(pool): State<PgPool>,
+) -> Result<Html<String>, (StatusCode, String)> {
     println!("/search: {:?}", params);
-    let result = params
-        .search
-        .split(',')
+    let mut results: Vec<String> = params.search.split(',').map(|s| s.to_string()).collect();
+
+    let sql_result: String = sqlx::query_scalar("select 'hello world from pg'")
+        .fetch_one(&pool)
+        .await
+        .map_err(internal_error)?;
+    println!("sql_result: {sql_result}");
+    results.push(sql_result);
+    println!("results: {:?}", results);
+
+    let result = results
+        .iter()
         .map(|s| format!("<li>{s}</li>"))
         .collect::<Vec<_>>()
         .join("\n");
-    Html(result)
+    Ok(Html(result))
 }
