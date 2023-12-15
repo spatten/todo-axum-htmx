309b085816882d00b8eba4c7e664c9126bb1048e
spatten <scott@fossa.com>
2023-12-15 00:15:39 -0800

    revert changes to sorting setup

--- client/myscript.js
+++ client/myscript.js
@@ -3,31 +3,21 @@ htmx.onLoad(function () {
   document.body.addEventListener("todoFormReset", function (evt) {
     document.getElementById('create-todo-form').reset();
   })
-  console.log("here I am");
 
-  function setupSortable() {
-    var sortable = document.getElementById("todos");
-    var sortableInstance = new Sortable(sortable, {
-      animation: 150,
-      ghostClass: 'blue-background-class',
-
-      // Disable sorting on the `end` event
-      onEnd: function (evt) {
-        console.log(`onEnd triggered.`);
-        // this.option("disabled", true);
-        htmx.trigger("#todos", "drop-end")
-      }
-    });
-
-    return sortable;
-  }
-  sortable = setupSortable();
-
-  sortable.addEventListener("htmx:afterSwap", function () {
-    console.log("re-enabling sort")
-    setupSortable();
-  });
   // Sorting of todos
+  var sortable = document.getElementById("todos");
+  var sortableInstance = new Sortable(sortable, {
+    animation: 150,
+    ghostClass: 'blue-background-class',
+
+    // Disable sorting on the `end` event
+    onEnd: function (evt) {
+      this.option("disabled", true);
+    }
+  });
 
   // Re-enable sorting on the `htmx:afterSwap` event
+  sortable.addEventListener("htmx:afterSwap", function () {
+    sortableInstance.option("disabled", false);
+  });
 })
