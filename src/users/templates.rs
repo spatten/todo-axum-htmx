6b3fc26bb399233127c92fc58926a3e6bd035f77
spatten <scott@fossa.com>
2023-12-26 19:59:42 -0800

    rename users_create template

--- src/users/templates.rs
+++ src/users/templates.rs
@@ -6,20 +6,20 @@ use crate::BaseTemplate;
 #[template(path = "users_new.html")]
 pub struct UsersNewTemplate<'a> {
     _parent: &'a BaseTemplate,
-    users_create: UsersCreateTemplate,
+    users_form: UsersFormTemplate,
 }
 
 pub fn render_new<'a>() -> UsersNewTemplate<'a> {
     UsersNewTemplate {
         _parent: &BaseTemplate {},
-        users_create: render_user_create(),
+        users_form: render_user_create(),
     }
 }
 
 #[derive(Template)]
-#[template(path = "users_create.html")]
-pub struct UsersCreateTemplate {}
+#[template(path = "users_form.html")]
+pub struct UsersFormTemplate {}
 
-pub fn render_user_create() -> UsersCreateTemplate {
-    UsersCreateTemplate {}
+pub fn render_user_create() -> UsersFormTemplate {
+    UsersFormTemplate {}
 }
